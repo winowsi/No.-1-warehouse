295da82207cfbb33d669198c440ee1ad0ec7b770
YouthDreamStation <351967265@qq.com>
2021-09-18 16:55:39 +0800

    project Environment to build

--- store-product/src/main/java/com/winowsi/product/StoreProductApplication.java
+++ store-product/src/main/java/com/winowsi/product/StoreProductApplication.java
@@ -1,9 +1,11 @@
 package com.winowsi.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.winowsi.product.dao")
 public class StoreProductApplication {
 
     public static void main(String[] args) {
--- store-product/src/test/java/com/winowsi/product/StoreProductApplicationTests.java
+++ store-product/src/test/java/com/winowsi/product/StoreProductApplicationTests.java
@@ -1,13 +1,27 @@
 package com.winowsi.product;
 
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.winowsi.product.entity.BrandEntity;
+import com.winowsi.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class StoreProductApplicationTests {
-
+    @Autowired
+    public BrandService service;
     @Test
-    void contextLoads() {
+    void fands() {
+
+        service.list(new QueryWrapper<BrandEntity>().eq("brand_id",1L)).forEach((brandEntity)->{
+            System.out.println(brandEntity);
+        });
+
     }
 
 }
